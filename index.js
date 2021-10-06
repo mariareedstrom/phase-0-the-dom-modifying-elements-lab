5b63997c9a0e5627eab3748880da494ca1fc31bb
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-10-06 10:59:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const element =document.getElementById("main");
+element.remove("main");
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Maria is the champion!"; 
+
+document.body.append(newHeader);
+
+
+
